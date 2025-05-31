d2b60497215e4a6ff21b772d4f56657fda609ecb
lucian diaconu <luciandiaconu123@gmail.com>
2025-05-31 22:48:18 +0200

    fix: add missing import for StandardCharsets in AuthenticationService

--- src/main/java/org/lucian/todos/service/AuthenticationService.java
+++ src/main/java/org/lucian/todos/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package org.lucian.todos.service;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
